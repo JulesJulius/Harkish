b16b92c047260d62615893e8df2c3adb33841b09
JulesJulius <JulesJulius71@gmail.com>
2023-05-13 19:51:38 +0000

    updates to news module

--- modules/news_module.py
+++ modules/news_module.py
@@ -1,18 +1,27 @@
 import praw
 import discord
 import asyncio
+import pickle
 from datetime import datetime, timedelta
 from . import Module
 
 class NewsModule(Module):
     def __init__(self, client, reddit_client_id, reddit_client_secret, reddit_user_agent):
         super().__init__(client)
+        
+        self.news_channel = 'news'
+
         self.reddit = praw.Reddit(
             client_id=reddit_client_id,
             client_secret=reddit_client_secret,
             user_agent=reddit_user_agent
         )
-        self.last_rising_posts = []
+        self.posted_news_file = "posted_news.pkl"
+        try:
+            with open(self.posted_news_file, "rb") as file:
+                self.posted_news = pickle.load(file)
+        except (FileNotFoundError, EOFError):
+            self.posted_news = set()
 
     async def on_ready(self):
         await self.check_rising_news()
@@ -25,26 +34,22 @@ class NewsModule(Module):
     async def check_rising_news(self):
         subreddit_name = "technews"
         subreddit = self.reddit.subreddit(subreddit_name)
-        rising_posts = subreddit.rising(limit=5)
+        rising_posts = subreddit.rising(limit=1)
 
-        new_post = next(rising_posts, None)
-        if new_post:
-            if not self.is_duplicate(new_post):
+        for new_post in rising_posts:
+            if new_post.id not in self.posted_news:
                 await self.post_news(new_post)
 
-    def is_duplicate(self, new_post):
-        top_post = self.last_rising_posts[0] if self.last_rising_posts else None
-        return top_post and new_post.title == top_post.title
-
     async def post_news(self, new_post):
-        channel_name = "news"  # Update with the name of your channel
+        channel_name = self.news_channel  # Update with the name of your channel
         guild = discord.utils.get(self.client.guilds, name="Tinker.ai")  # Update with your guild name
         channel = discord.utils.get(guild.channels, name=channel_name)
         if channel:
             await channel.send(f"New rising news article: {new_post.title}\n{new_post.url}")
 
-        self.last_rising_posts.insert(0, new_post)
-        self.last_rising_posts = self.last_rising_posts[:5]  # Keep track of only the last 5 rising posts
+        self.posted_news.add(new_post.id)
+        with open(self.posted_news_file, "wb") as file:
+            pickle.dump(self.posted_news, file)
 
     async def on_message(self, message):
         if message.content.startswith('!news'):
